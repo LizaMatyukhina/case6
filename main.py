667096dbd03f70420a75517625a5cfa299b093e3
alina__romanenko <alinaromanenko1@yandex.ru>
2019-05-14 20:40:11 +0700

    смерть 1 клетки, конец игры при зацикливании предыдущего шага

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from tkinter import *
-#from contracts import contract
+from contracts import contract
+
 
 class Cell(Button):
 
-    #name - положение клетки!
+    # name - положение клетки!
     # 1 цирфа это по Х, вторая по У
     def __init__(self, x, y, size, name):
         super(Cell, self).__init__(root,
@@ -23,36 +24,65 @@ class Cell(Button):
         self.alive = True
         self.configure(bg='white')
 
+    def killer(self):
+        self.alive = False
+        self.configure(bg='black')
 
-#@contract вот с ним я не совсем разобралась но вот попыталсь прописать все тесты максимально возможные
 
+@contract
 # функция создающая все клетки на поле, они уже сразу объекты типа Клетка
 def background(size):
     '''
-    :type size: int, < 40, not zero
-    :rtype: list(str)
+    :type size: int, <=40, != 0
+    :rtype: list
     '''
     cells = []
     for j in range(0, 500, size):
         for k in range(0, 400, size):
-            our_cell = Cell(j, k, size, str(int(j / size + 1)) + ',' + str(int(k / size + 1)))
+            our_cell = Cell(j, k, size, [int(j / size + 1), int(k / size + 1)])
             cells.append(our_cell)
     return cells
 
-#@contract
-# здесь поределяем все живые клетки на поле в самом начале
+
+# @contract
 def live_cells(cells):
     """
     :param cells: list, not null
-    :rtype: list(tuple)
+    :type cells: list
+    :rtype: list
     """
     living = []
     for cell in cells:
         if cell.alive:
-            living.append((cell, cell.name))
-    # living содержит набор кортежей, в которых первый элемент это сама клетка, а второй это ее положение, т. е. строка и столбец
+            living.append([cell, cell.name])
     print(living)
+    killing(living)
 
+@contract
+def killing(living):
+    '''
+    :type living: list
+    '''
+    while living:
+        old_living = living
+        for white_cell in living:
+            neighbours = 0
+            l_x = white_cell[1][0]
+            l_y = white_cell[1][1]
+            for neighbour in living:
+                n_x = neighbour[1][0]
+                n_y = neighbour[1][1]
+                if (n_x - l_x) + (n_y - l_y) != 0:
+                    if abs(n_x - l_x) < 2 and abs(n_y - l_y) < 2:
+                        neighbours += 1
+            if neighbours < 2 or neighbours > 3:
+                white_cell[0].killer()
+                living.remove(white_cell)
+            if old_living == living:
+                for white_cell in living:
+                    white_cell[0].killer()
+                living = []
+                break
 
 
 root = Tk()
